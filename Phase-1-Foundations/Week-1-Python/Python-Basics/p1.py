6e1c3b57cdd9fa55591320a205927a290a62c49b
Rudra <rhpatel1023@gmail.com>
2025-11-30 20:54:30 +0530

    practiced match and loop

--- Phase-1-Foundations/Week-1-Python/Python-Basics/p1.py
+++ Phase-1-Foundations/Week-1-Python/Python-Basics/p1.py
@@ -1,3 +1,30 @@
-print("Hello World!")
-print(7,"hello",9,sep="~")
-print(5**2)
+# print("Hello World!")
+# print(7,"hello",9,sep="~")
+# print(5**2)
+# a='hello-Bhai'
+# print(a.split('-'))
+# b="hellooooo            "
+# print(b.rstrip().rstrip('o')+'b')
+# c='this Is my laptop. Who Are You?'
+# print(c.capitalize())
+# d='    t    '
+# print(d.isspace())
+
+
+# x=5
+# match x:
+#     case 1:
+#         print(1)
+#     case 2:
+#         print(2)
+#     case 3:
+#         print(3)
+#     case 4:
+#         print(4)
+#     case _:
+#         print(5)
+
+# for i in range(1,5,2):
+#     print(i)
+
+
