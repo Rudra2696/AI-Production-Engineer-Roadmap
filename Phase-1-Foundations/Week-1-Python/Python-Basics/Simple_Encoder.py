55aa5abdd16100cad0d4ffbd992b19c8369bbf01
Rudra <rhpatel1023@gmail.com>
2025-12-03 19:17:12 +0530

    it encodes the message by simple mehod  which dependends on it's length

---
+++ Phase-1-Foundations/Week-1-Python/Python-Basics/Simple_Encoder.py
@@ -0,0 +1,19 @@
+n=input("Enter your message : ")
+if(len(n)<3):
+    
+    s=n[1]+n[0]
+else:
+    s=''
+    l=[i for i in n]
+    s1=l[0]
+    l.append(s1)
+    l.pop(0)
+    l.insert(0,'z')
+    l.insert(1,'e')
+    l.insert(2,'d')
+    l.append('i')
+    l.append('p')
+    l.append('o')
+    for i in l:
+        s+=i
+print(s)
